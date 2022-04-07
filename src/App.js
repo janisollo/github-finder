550de9738f533f7ed70f0a1ebe2b1a83e8c2606d
janisollo <sollo.jani@gmail.com>
2022-04-07 17:31:11 +0700

    File structure done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App  extends Component {
+  
+  render() {
+    const name = {
+      firstName: "Jani",
+      lastName: "Sollo"
+  }
+
+    return(
+
+    <Fragment>
+      <h1>Hello {name.firstName.toUpperCase()}</h1>
+    </Fragment>
+
+
+    )
+  }
 }
 
 export default App;
